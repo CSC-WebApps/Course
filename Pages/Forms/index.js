27c06235b6a138143353b2258a78f1e14638c4d8
Christopher J. Parnin <chris.parnin@gmail.com>
2021-09-02 15:14:57 -0400

    testing get paths.

--- Pages/Forms/index.js
+++ Pages/Forms/index.js
@@ -12,6 +12,15 @@ app.post('/submit-form', (req, res) => {
     res.end()
 })
 
+app.get('/CSC-WebApps/Course/Pages/Forms/Forms.md/env/port/3000', (req, res) => {
+  res.send("hello")
+})
+
+app.get('/', (req, res) => {
+  res.send("hello")
+})
+
+
 app.post('/CSC-WebApps/Course/Pages/Forms/Forms.md/env/port/3000', (req, res) => {
   const username = req.body.username
   console.log( username );
