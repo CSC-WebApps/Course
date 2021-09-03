fbc50cbec33bf20f17de4933742b1b4cc3c65f5f
Chris Parnin <chris.parnin@gmail.com>
2021-09-02 22:17:27 -0400

    body-parser?

--- Pages/Forms/index.js
+++ Pages/Forms/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
-app.use(express.json()); 
-app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/submit-form', (req, res) => {
     const username = req.body.username
