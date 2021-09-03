9bcfdd589cc50a128d50ba27bcf34c5fd870e9b1
Chris Parnin <chris.parnin@gmail.com>
2021-09-02 22:04:40 -0400

    exstended

--- Pages/Forms/index.js
+++ Pages/Forms/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.use(express.json()); 
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 
 app.post('/submit-form', (req, res) => {
     const username = req.body.username
