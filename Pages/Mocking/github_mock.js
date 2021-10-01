63b82841db4b67f22bf837bce779ad8c76726b0f
Christopher J. Parnin <chris.parnin@gmail.com>
2021-10-01 11:49:50 -0400

    data

--- Pages/Mocking/github_mock.js
+++ Pages/Mocking/github_mock.js
@@ -1,5 +1,7 @@
 const nock = require('nock');
 
+let data = require('./mock.json');
+
 var mockService = nock("https://api.github.com")
     .persist() // This will persist mock interception for lifetime of program.
     .get("/repos/testuser/Hello-World/issues")
