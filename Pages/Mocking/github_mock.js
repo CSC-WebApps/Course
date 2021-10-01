f7a2cecedcabac1b3a4dfbd2a736d86e8ba02c22
Christopher J. Parnin <chris.parnin@gmail.com>
2021-10-01 12:04:43 -0400

    correct headers

--- Pages/Mocking/github_mock.js
+++ Pages/Mocking/github_mock.js
@@ -4,6 +4,9 @@ let data = require('./mock.json');
 
 var mockService = nock("https://api.github.com")
     .persist() // This will persist mock interception for lifetime of program.
+    .defaultReplyHeaders({
+      'Content-Type': 'application/json',
+    })    
     .get("/repos/testuser/Hello-World/issues")
     .reply(200, data.issueList );
 
